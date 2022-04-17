fc3c4ffd25d2c4872fc3793a1a50a1f5380cd1e4
Václav Doležal <bc.va.do@gmail.com>
2022-04-17 11:01:40 +0200

    two attitudes to solve "AVERAGE"

---
+++ python02.py
@@ -0,0 +1,21 @@
+
+
+#user_numer = int(input("Enter number one: "))
+#user_number = int(Input("Enter number two: "))
+
+numbers = [10,15,20,40]
+print(sum(numbers))
+sum_of_numbers = sum(numbers)
+print(len(numbers))
+numbers_count = len(numbers)
+
+average01 = sum_of_numbers/numbers_count
+print(average01)
+
+import statistics
+numbers = [10,15,20,40]
+
+average02 = statistics.mean(numbers)
+print(average02)
+
+
