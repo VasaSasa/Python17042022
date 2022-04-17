9a574385667c4e207a68bebecf2626f9d83eec7f
Václav Doležal <bc.va.do@gmail.com>
2022-04-17 12:24:49 +0200

    another example of solving sum and everage

--- python02.py
+++ python02.py
@@ -1,21 +1,50 @@
 
 
-#user_numer = int(input("Enter number one: "))
-#user_number = int(Input("Enter number two: "))
+#TASK
 
-numbers = [10,15,20,40]
-print(sum(numbers))
-sum_of_numbers = sum(numbers)
-print(len(numbers))
-numbers_count = len(numbers)
+#numbers = [10,15,20,40]
+#print(sum(numbers))
+#sum_of_numbers = sum(numbers)
+#print(len(numbers))
+#numbers_count = len(numbers)
 
-average01 = sum_of_numbers/numbers_count
-print(average01)
+#average01 = sum_of_numbers/numbers_count
+#print(average01)
 
-import statistics
-numbers = [10,15,20,40]
+#import statistics
+#numbers = [10,15,20,40]
 
-average02 = statistics.mean(numbers)
-print(average02)
+#average02 = statistics.mean(numbers)
+#print(average02)
+
+#TASK
+
+#user_number01 = int(input("Enter number one: "))
+#user_number02 = int(input("Enter number two: "))
 
 
+#total_sum = 0
+#amount = 0
+
+#for i in range(user_number01,user_number02 + 1):
+    #total_sum += i
+    #amount +=1
+
+#print(total_sum/amount)
+
+#numbers = []
+
+#while True:
+    #number = int(input("Enter number: "))
+    #numbers.append(number)
+    #if input("For END press „y“ ") == "y":
+        #break
+
+#import statistics
+
+#average01 = statistics.mean(numbers)
+#print(statistics.mean(numbers))
+#print(round(average01,2))
+
+#cislo = 1.6666666
+#print(round(cislo,2))
